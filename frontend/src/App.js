85e0c82fac31625e1077050ce6d7c281baa20e58
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-10-08 13:44:44 -0300

    adding-settings

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>FUNCIONANDO</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/redux/reducer/index.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    products: []
+  };
+
+  export default function rootReducer(state = initialState, action) {
+
+   
+  
+  }
+       
+  
+  
+  
+  
